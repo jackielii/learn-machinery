323133459bac02950d03700e4a12010a7b11c8ba
Jackie Li <jackie.li@fuseim.com>
2019-04-01 20:18:47 +0800

    example

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module learn-machinery
 
 go 1.12
+
+require (
+	github.com/RichardKnop/machinery v1.6.0
+	github.com/davecgh/go-spew v1.1.1
+)
--- main.go
+++ main.go
@@ -1,5 +1,78 @@
 package main
 
-func main() {
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"time"
 
+	"github.com/RichardKnop/machinery/v1"
+	"github.com/RichardKnop/machinery/v1/config"
+	"github.com/RichardKnop/machinery/v1/tasks"
+	"github.com/davecgh/go-spew/spew"
+)
+
+func main() {
+	cfg := &config.Config{
+		Broker:        "redis://localhost:6379",
+		ResultBackend: "redis://localhost:6379",
+	}
+	server, err := machinery.NewServer(cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// tasks := map[string]interface{}{
+	//     "task": task,
+	// }
+	// server.RegisterTasks(tasks)
+	server.RegisterTask("task", task)
+
+	worker := server.NewWorker("worker1", 10)
+
+	errCh := make(chan error)
+	worker.LaunchAsync(errCh)
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+
+	sig := tasks.Signature{
+		Name: "task",
+		Args: []tasks.Arg{
+			{
+				Type:  "string",
+				Value: "hello please",
+			},
+		},
+	}
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	asyncResult, err := server.SendTaskWithContext(ctx, &sig)
+	if err != nil {
+		log.Fatal(err)
+	}
+	spew.Dump("state before ", asyncResult.GetState())
+	// rs, err := asyncResult.Get(10 * time.Millisecond)
+	rs, err := asyncResult.GetWithTimeout(1000*time.Millisecond, 10*time.Millisecond)
+	spew.Dump("state after ", asyncResult.GetState())
+
+	for _, r := range rs {
+		spew.Dump("results", r)
+	}
+	spew.Dump("results: ", tasks.HumanReadableResults(rs))
+
+	worker.Quit()
+	err = <-errCh
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func task(ctx context.Context, msg string) (string, error) {
+	fmt.Println("received ", msg)
+
+	return "returning from task", nil
 }
